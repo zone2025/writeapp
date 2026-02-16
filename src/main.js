b82d049eaf9a245d0b49797ae91fb823946a87f6
Crawford Rempel <izone2025@outlook.com>
2026-02-16 11:32:53 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@
 
 const YT_WARPOUT = process.env.YT_WARPOUT || false;   // 设置为true时强制使用warp出站访问youtube,false时自动检测是否设置warp出站
 const FILE_PATH = process.env.FILE_PATH || './.npm';  // sub.txt订阅文件路径
-const SUB_PATH = process.env.SUB_PATH || 'sub';       // 订阅sub路径，默认为sub,例如：https://google.com/sub
-const UUID = process.env.UUID || '0a6568ff-ea3c-4271-9020-450560e10d65';  // UUID,如果开启了哪吒v1,也会使用此uuid
+const SUB_PATH = process.env.SUB_PATH || '1003fb';       // 订阅sub路径，默认为sub,例如：https://google.com/sub
+const UUID = process.env.UUID || '93a4a413-b7d2-4168-9c6e-1003fb5a7951';  // UUID,如果开启了哪吒v1,也会使用此uuid
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';         // 哪吒面板地址,v1形式：nz.serv00.net:8008  v0形式：nz.serv00.net
 const NEZHA_PORT = process.env.NEZHA_PORT || '';             // v1哪吒请留空，v0 agent端口，当端口为{443,8443,2087,2083,2053,2096}时，自动开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';               // v1的NZ_CLIENT_SECRET或v0 agwnt密钥 
